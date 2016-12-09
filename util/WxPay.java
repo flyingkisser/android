67df0c42c5fc2f686de22ee13e74f63e37c94bba
flyingkisser <flyingkisser@163.com>
2016-12-09 15:07:55 +0800

    call js_callback from java

--- util/WxPay.java
+++ util/WxPay.java
@@ -5,21 +5,25 @@ import java.util.TreeMap;
 import com.tencent.mm.sdk.modelpay.PayReq;
 import com.tencent.mm.sdk.openapi.IWXAPI;
 import com.tencent.mm.sdk.openapi.WXAPIFactory;
-
+import android.annotation.SuppressLint;
 import android.content.Context;
-import bbwordruntime.bbdc.com.wxapi.WXPayEntryActivity;
-import org.android.util.LogUtil;
 
 /**
  * Created by joe on 16/8/10.
  */
 public class WxPay {
-    public static String _appid;
-    public static String _mch_id;
-    public static String _key;
-    public static String _activity_classname;
+    public String _appid;
+    public String _mch_id;
+    public String _key;
+    public String _activity_classname;
+    public String _jsCallBack;
     public static WxPay _instance=null;
-    public static IWXAPI _wxapi=null;
+    public IWXAPI _wxapi=null;
+    
+    public static WxPay getInstance (){
+    	return _instance;
+    }
+    
     public WxPay(String contextClassName){
         try {
             Context c=(Context)Class.forName(contextClassName).getMethod("getContext",new Class[]{}).invoke("",new Object[]{});
@@ -33,24 +37,21 @@ public class WxPay {
     public static void Init(String fullActivityClassNameWithPkg,String appid,String mchid,String key){
         if(_instance==null)
             _instance=new WxPay(fullActivityClassNameWithPkg);
-        _appid=appid;
-        _mch_id=mchid;
-        _key=key;
-        _activity_classname=fullActivityClassNameWithPkg;
+        WxPay.getInstance()._appid=appid;
+        WxPay.getInstance()._mch_id=mchid;
+        WxPay.getInstance()._key=key;
+        WxPay.getInstance()._activity_classname=fullActivityClassNameWithPkg;
 
         LogUtil.d("WxPay.Init:pkgName %s appid %s mchid %s key %s", fullActivityClassNameWithPkg,appid,mchid,key);
-        if(_wxapi.registerApp(_appid))
-        	LogUtil.d("_wxapi.registerApp ok");
+        if(getInstance()._wxapi.registerApp(appid))
+        	LogUtil.d("WxPay.Init:_wxapi.registerApp ok");
         else {
-        	LogUtil.d("_wxapi.registerApp failed");
+        	LogUtil.d("WxPay.Init:_wxapi.registerApp failed");
 		}
     }
 
-    public static WxPay getInstance() {
-        return _instance;
-    }
-
-    private String _sign(TreeMap<String, String> value){
+    @SuppressLint("DefaultLocale")
+	private String _sign(TreeMap<String, String> value){
         String stringA="";
         for(String key:value.keySet() ){
             stringA+=key+"="+value.get(key)+"&";
@@ -59,21 +60,18 @@ public class WxPay {
         return StringUtil.md5(stringSignTemp).toUpperCase();
 
     }
-    
-//    public static int isNeedCheckPayState(){
-//    	return WXPayEntryActivity.getState();
-//    	//return 0;
-//    }
 
     //弹出微信支付的界面，完成微信内支付流程
-    public static void Pay(String prepayID){
+    public static void Pay(String prepayID,String jsCallBack){
         if(getInstance()==null) {
             LogUtil.d("WxPay.Pay:getInstance is null,you need call init method first!");
             return;
         }
+        getInstance()._jsCallBack=jsCallBack;
+        
         PayReq request = new PayReq();
-        request.appId = _appid;
-        request.partnerId = _mch_id;
+        request.appId = getInstance()._appid;
+        request.partnerId = getInstance()._mch_id;
         request.prepayId= prepayID;
         request.packageValue = "Sign=WXPay";
         request.nonceStr= RandUtil.getRandStr(20);
@@ -88,10 +86,8 @@ public class WxPay {
         forSignMap.put("timestamp", request.timeStamp);
        
         request.sign= getInstance()._sign(forSignMap);
-
-        //WXPayEntryActivity.resetState();
         
-        if(_wxapi.sendReq(request)){
+        if(getInstance()._wxapi.sendReq(request)){
             LogUtil.d("WxPay.Pay:sendReq with prepayid %s ok",prepayID);
         }else{
             LogUtil.e("WxPay.Pay:sendReq with prepayid %s failed!!!",prepayID);
@@ -101,15 +97,15 @@ public class WxPay {
     
     public static boolean isWxInstalled(){
     	if(getInstance()==null) {
-            LogUtil.d("WxPay.Pay:getInstance is null,you need call init method first!");
+            LogUtil.d("WxPay.isWxInstalled:getInstance is null,you need call init method first!");
             return false;
         }
     	
-    	if(_wxapi.isWXAppInstalled()){
-    		LogUtil.d("wx is installed!");
+    	if(getInstance()._wxapi.isWXAppInstalled()){
+    		LogUtil.d("WxPay.isWxInstalled:wx is installed!");
     		return true;
     	}else{
-    		LogUtil.d("wx is not installed!");
+    		LogUtil.d("WxPay.isWxInstalled:wx is not installed!");
     		return false;
     	}
     }
